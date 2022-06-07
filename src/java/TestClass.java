88264b7953140b0d86e48c08d898f76ca0c1fd1d
Michael Z <zaloginmiha@gmail.com>
2022-06-07 20:55:17 +0300

    first commit from terminal

--- src/java/TestClass.java
+++ src/java/TestClass.java
@@ -4,5 +4,6 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("Test git1");
         System.out.println("Test git2");
+        System.out.println("Test git3");
     }
 }
