81f3fca2083fb6f6e541bf8b2f67a0569eaa9557
Michael Z <zaloginmiha@gmail.com>
2022-06-11 01:11:14 +0300

    Gist

---
+++ src/TestGist.java
@@ -0,0 +1,6 @@
+public class TestGist {
+    public static void main(String[] args) {
+        System.out.println("TestGist");
+        ///check Gist: https://gist.github.com/MichaelZalogin
+    }
+}
