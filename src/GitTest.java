07e84d25f5978f609f2b4a18433ba5f87b9e6755
MichaelZalogin <zaloginmiha@gmail.com>
2022-06-11 18:42:22 +0300

    TestBranch

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,20 +1,5 @@
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("GitHub test");
-        System.out.println("GitHub test");
-        System.out.println("GitHub test");
-        System.out.println("GitHub test1");
-        System.out.println("GitHub test2");
-        System.out.println("GitHub test3");
-        System.out.println("GitHub test4");
-        System.out.println("GitHub test4");
-        System.out.println("GitHub test4");
-        System.out.println("GitHub test4");
-        System.out.println("GitHub test4");
-        System.out.println("GitHub test4");
-        System.out.println("GitHub test4");
-        System.out.println("GitHub test4");
-        System.out.println("Develop2Branch");
-        System.out.println("GitHub test4");
+        System.out.println("Тестирование ветвей");
     }
 }
