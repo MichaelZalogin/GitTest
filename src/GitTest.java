2ae3be3361a6fd093f8c301bfe304a6c6d464031
MichaelZalogin <zaloginmiha@gmail.com>
2022-06-11 18:44:19 +0300

    TestBranch

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,5 +1,9 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Тестирование ветвей");
+        System.out.println("Локальное изменение в ветке develop #1");
+        System.out.println("Локальное изменение в ветке develop #2");
+        System.out.println("Локальное изменение в ветке develop #3");
+        System.out.println("Локальное изменение в ветке develop #4");
     }
 }
